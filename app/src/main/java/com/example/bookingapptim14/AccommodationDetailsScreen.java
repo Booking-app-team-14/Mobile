489b4b0082d1f8ea3622f9c37e9b0a01c8cb824d
Nikola Mitrovic <mnikola300@gmail.com>
2023-11-16 22:31:29 +0100

    added Search Bar

---
+++ app/src/main/java/com/example/bookingapptim14/AccommodationDetailsScreen.java
@@ -0,0 +1,13 @@
+  package com.example.bookingapptim14;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccommodationDetailsScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accommodation_details); // Replace with your layout file
+        // Other initialization code or logic
+    }
+}
--- app/src/main/java/com/example/bookingapptim14/HomeFragment.java
+++ app/src/main/java/com/example/bookingapptim14/HomeFragment.java
@@ -10,29 +10,26 @@ import android.view.ViewGroup;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
 public class HomeFragment extends Fragment {
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_home, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-            // Find the CardView in your layout
-            CardView cardView = view.findViewById(R.id.cardView);
+        // Find the view (e.g., Button, CardView) in your layout
+        CardView cardView = view.findViewById(R.id.cardView);
 
-            // Set OnClickListener for the CardView
-            cardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Replace the current fragment with the new fragment (replace YourFragment.class with the desired fragment)
-                    AccommodationDetailsFragment fragment = new AccommodationDetailsFragment(); // Replace with your fragment
-                    FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.homeFragment, fragment); // Replace the R.id.homeFragment with your FrameLayout ID
-                    fragmentTransaction.addToBackStack(null);
-                    fragmentTransaction.commit();
-                }
-            });
+        // Set OnClickListener for the view
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the AccommodationDetailsScreen activity
+                Intent intent = new Intent(getActivity(), AccommodationDetailsScreen.class);
+                startActivity(intent);
+            }
+        });
 
-            return view;
-        }
+        return view;
     }
+}
