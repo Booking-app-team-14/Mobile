f79e50b50b8071470b2fab115cbe036beb87f194
Nikola Mitrovic <mnikola300@gmail.com>
2023-11-18 19:51:49 +0100

    made a reservation GUI

--- app/src/main/java/com/example/bookingapptim14/AccommodationDetailsScreen.java
+++ app/src/main/java/com/example/bookingapptim14/AccommodationDetailsScreen.java
@@ -1,19 +1,46 @@
-  package com.example.bookingapptim14;
+package com.example.bookingapptim14;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 
-import androidx.cardview.widget.CardView;
+public class AccommodationDetailsScreen extends Activity {
 
-  public class AccommodationDetailsScreen extends Activity {
+    Button bookingButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accommodation_details);
 
+        bookingButton = findViewById(R.id.common_button);
+        bookingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFilterDialog();
+            }
+        });
+    }
 
+    private void showFilterDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.activity_reservation_request_guest, null);
+        builder.setView(dialogView);
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // Cancel action
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
