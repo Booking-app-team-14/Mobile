d6a764c8140a97f1eaf7172bdc245834aab11511
marijazhiv <105136036+marijazhiv@users.noreply.github.com>
2023-11-19 10:26:00 +0100

    Log in and Register features implemented

--- app/src/main/java/com/example/bookingapptim14/LoginScreen.java
+++ app/src/main/java/com/example/bookingapptim14/LoginScreen.java
@@ -2,15 +2,21 @@ package com.example.bookingapptim14;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorInflater;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.bookingapptim14.admin.MainActivityAdmin;
 import com.example.bookingapptim14.guest.MainActivityGuest;
+import com.example.bookingapptim14.guest.ProfileFragmentGuest;
 import com.example.bookingapptim14.host.MainActivityHost;
 
 public class LoginScreen extends AppCompatActivity {
@@ -20,48 +26,57 @@ public class LoginScreen extends AppCompatActivity {
     private Button loginButton;
     private Button registerButton;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-        usernameEditText = findViewById(R.id.usernameEditText);
-        passwordEditText = findViewById(R.id.passwordEditText);
-        loginButton = findViewById(R.id.loginButton);
-        registerButton = findViewById(R.id.registerButton);
+        // Inicijalizacija elemenata sa layout-a
+        usernameEditText = findViewById(R.id.editTextEmail);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        loginButton = findViewById(R.id.buttonLogin);
+        TextView signUpLink = findViewById(R.id.textViewSignUp);
 
+        ImageView imageView = findViewById(R.id.imageView);
+        ObjectAnimator rotationAnimator = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.rotate_animation);
+        rotationAnimator.setTarget(imageView);
+        rotationAnimator.start();
+
+        // Postavljanje veličine ikonice za email EditText
+        Drawable emailIcon = getResources().getDrawable(R.drawable.img_4);
+        emailIcon.setBounds(15, 1, (int) (emailIcon.getIntrinsicWidth() * 0.06), (int) (emailIcon.getIntrinsicHeight() * 0.06));
+        usernameEditText.setCompoundDrawables(emailIcon, null, null, null);
+
+        Drawable passwordIcon = getResources().getDrawable(R.drawable.img_6);
+        passwordIcon.setBounds(15, 1, (int) (passwordIcon.getIntrinsicWidth() * 0.07), (int) (passwordIcon.getIntrinsicHeight() * 0.05));
+        passwordEditText.setCompoundDrawables(passwordIcon, null, null, null);
+
+        // Dodajte OnClickListener za dugme za prijavu
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = null;
-                if (usernameEditText.getText().toString().equals("guest")){
-                    intent = new Intent(LoginScreen.this, MainActivityGuest.class);
-                }
-                else if (usernameEditText.getText().toString().equals("host")){
-                    intent = new Intent(LoginScreen.this, MainActivityHost.class);
-                }
-                else if (usernameEditText.getText().toString().equals("admin")){
-                    intent = new Intent(LoginScreen.this, MainActivityAdmin.class);
-                }
-                else{
-                    Toast.makeText(LoginScreen.this, "Enter username", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+            public void onClick(View v) {
+                // Ovde treba dodati kod za proveru unosa korisničkog imena i lozinke.
+                // Ako je unos ispravan, prebacite se na HomeScreenActivity.
+                Intent intent = new Intent(LoginScreen.this, ProfileFragmentGuest.class);
                 startActivity(intent);
-                finish();
             }
         });
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        // Dodajte OnClickListener za dugme za registraciju
+        signUpLink.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                // Ovde dodajte kod za prelazak na ekran za registraciju.
                 Intent intent = new Intent(LoginScreen.this, RegisterScreen.class);
                 startActivity(intent);
-                finish();
             }
         });
-
     }
 
+    // Ostale metode života aktivnosti...
+
+    public void onForgotPasswordClick(View view) {
+        // Ovde dodajte kod koji će se izvršiti kada korisnik klikne na "Forgot Password".
+        // Na primer, možete otvoriti novu aktivnost za resetovanje lozinke.
+    }
 }
--- app/src/main/java/com/example/bookingapptim14/RegisterScreen.java
+++ app/src/main/java/com/example/bookingapptim14/RegisterScreen.java
@@ -6,26 +6,48 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.bookingapptim14.guest.MainActivityGuest;
 
 
 public class RegisterScreen extends AppCompatActivity {
-    private Button confirmButton;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button registerButton;
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
-        Button confirmButton = findViewById(R.id.confirmButton);
-        confirmButton.setOnClickListener(new View.OnClickListener() {
+
+        // Inicijalizacija elemenata sa layout-a
+        usernameEditText = findViewById(R.id.editTextEmail);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        registerButton = findViewById(R.id.buttonRegister);
+        TextView loginLink = findViewById(R.id.textViewLogin);
+
+        // Dodajte OnClickListener za dugme za registraciju
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterScreen.this, MainActivityGuest.class);
+                // Ovde treba dodati kod za proces registracije.
+                // Nakon uspešne registracije, možete prebaciti korisnika na LoginScreenActivity.
+                Intent intent = new Intent(RegisterScreen.this, LoginScreen.class);
                 startActivity(intent);
-                finish();
             }
         });
 
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ovde dodajte kod za prelazak na ekran za registraciju.
+                Intent intent = new Intent(RegisterScreen.this, LoginScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
