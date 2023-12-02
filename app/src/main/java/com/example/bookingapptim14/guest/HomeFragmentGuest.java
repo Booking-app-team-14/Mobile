0f28de2cb559d7403172933b9b05d7c8cc26f891
Nikola Mitrovic <mnikola300@gmail.com>
2023-12-02 22:28:13 +0100

    added more filtering options

--- app/src/main/java/com/example/bookingapptim14/guest/HomeFragmentGuest.java
+++ app/src/main/java/com/example/bookingapptim14/guest/HomeFragmentGuest.java
@@ -6,17 +6,27 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import com.example.bookingapptim14.R;
+import com.google.android.material.datepicker.MaterialDatePicker;
+
+import java.util.Calendar;
 
 public class HomeFragmentGuest extends Fragment {
+
+        private TextView textViewDateRange;
+        private Button btnDateRangePicker;
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View view = inflater.inflate(R.layout.fragment_home_guest, container, false);
@@ -29,6 +39,8 @@ public class HomeFragmentGuest extends Fragment {
                     isFavorite[0] = !isFavorite[0];
 
                     heartButton.setSelected(isFavorite[0]);
+
+
 
                 }
             });
@@ -61,6 +73,16 @@ public class HomeFragmentGuest extends Fragment {
 
             View dialogView = inflater.inflate(R.layout.filter_dialog, null);
 
+            NumberPicker numberPicker = dialogView.findViewById(R.id.numberPickerGuests);
+
+            numberPicker.setMinValue(1);
+            numberPicker.setMaxValue(10);
+
+            textViewDateRange = dialogView.findViewById(R.id.textViewDateRange);
+            btnDateRangePicker = dialogView.findViewById(R.id.btnDateRangePicker);
+
+            // Set OnClickListener to open Date Range Picker
+            btnDateRangePicker.setOnClickListener(v -> openDateRangePicker());
             builder.setView(dialogView)
                     .setPositiveButton("Apply", new DialogInterface.OnClickListener() {
                         @Override
@@ -77,5 +99,36 @@ public class HomeFragmentGuest extends Fragment {
             AlertDialog dialog = builder.create();
             dialog.show();
         }
+
+    private void openDateRangePicker() {
+        MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
+        builder.setTitleText("Select Dates");
+
+        Calendar startDate = Calendar.getInstance();
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.DAY_OF_MONTH, 1); // Default range of 1 day
+        builder.setSelection(new Pair<>(startDate.getTimeInMillis(), endDate.getTimeInMillis()));
+
+        MaterialDatePicker<Pair<Long, Long>> picker = builder.build();
+
+        picker.addOnPositiveButtonClickListener(selection -> {
+            long startMillis = selection.first;
+            long endMillis = selection.second;
+
+            Calendar startCal = Calendar.getInstance();
+            startCal.setTimeInMillis(startMillis);
+            Calendar endCal = Calendar.getInstance();
+            endCal.setTimeInMillis(endMillis);
+
+            String dateRange = String.format(
+                    "Check-in: %s\nCheck-out: %s",
+                    android.text.format.DateFormat.format("dd/MM/yyyy", startCal),
+                    android.text.format.DateFormat.format("dd/MM/yyyy", endCal)
+            );
+            textViewDateRange.setText(dateRange);
+        });
+
+        picker.show(getChildFragmentManager(), picker.toString());
     }
+}
 
--- app/src/main/java/com/example/bookingapptim14/host/HomeFragmentHost.java
+++ app/src/main/java/com/example/bookingapptim14/host/HomeFragmentHost.java
@@ -7,22 +7,31 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.PopupMenu;
 
 import androidx.cardview.widget.CardView;
 
 import com.example.bookingapptim14.R;
+import com.google.android.material.datepicker.MaterialDatePicker;
+
+import java.util.Calendar;
 
 
 public class HomeFragmentHost extends Fragment {
 
+    private TextView textViewDateRange;
+    private Button btnDateRangePicker;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_host, container, false);
@@ -55,7 +64,16 @@ public class HomeFragmentHost extends Fragment {
         LayoutInflater inflater = requireActivity().getLayoutInflater();
 
         View dialogView = inflater.inflate(R.layout.filter_dialog, null);
+        NumberPicker numberPicker = dialogView.findViewById(R.id.numberPickerGuests);
 
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(10);
+
+        textViewDateRange = dialogView.findViewById(R.id.textViewDateRange);
+        btnDateRangePicker = dialogView.findViewById(R.id.btnDateRangePicker);
+
+        // Set OnClickListener to open Date Range Picker
+        btnDateRangePicker.setOnClickListener(v -> openDateRangePicker());
         builder.setView(dialogView)
                 .setPositiveButton("Apply", new DialogInterface.OnClickListener() {
                     @Override
@@ -101,6 +119,37 @@ public class HomeFragmentHost extends Fragment {
 
 
         popupMenu.show();
+    }
+
+    private void openDateRangePicker() {
+        MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
+        builder.setTitleText("Select Dates");
+
+        Calendar startDate = Calendar.getInstance();
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.DAY_OF_MONTH, 1); // Default range of 1 day
+        builder.setSelection(new Pair<>(startDate.getTimeInMillis(), endDate.getTimeInMillis()));
+
+        MaterialDatePicker<Pair<Long, Long>> picker = builder.build();
+
+        picker.addOnPositiveButtonClickListener(selection -> {
+            long startMillis = selection.first;
+            long endMillis = selection.second;
+
+            Calendar startCal = Calendar.getInstance();
+            startCal.setTimeInMillis(startMillis);
+            Calendar endCal = Calendar.getInstance();
+            endCal.setTimeInMillis(endMillis);
+
+            String dateRange = String.format(
+                    "Check-in: %s\nCheck-out: %s",
+                    android.text.format.DateFormat.format("dd/MM/yyyy", startCal),
+                    android.text.format.DateFormat.format("dd/MM/yyyy", endCal)
+            );
+            textViewDateRange.setText(dateRange);
+        });
+
+        picker.show(getChildFragmentManager(), picker.toString());
     }
 
 }
