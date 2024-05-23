c2c1a2be28ef2737ba7899d8ac2e0855cd7e1f10
Vuk <vuk.dimitrov@gmail.com>
2024-05-23 03:30:11 +0200

    Template for admin user and review blocking.

--- app/src/main/java/com/example/bookingapptim14/admin/ReportsFragmentAdmin.java
+++ app/src/main/java/com/example/bookingapptim14/admin/ReportsFragmentAdmin.java
@@ -3,18 +3,53 @@ package com.example.bookingapptim14.admin;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.bookingapptim14.Adapters.AdminReportsPagerAdapter;
+import com.example.bookingapptim14.Adapters.MyViewPagerAdapter;
 import com.example.bookingapptim14.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class ReportsFragmentAdmin extends Fragment {
 
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_reports_admin, container, false);
+        View view = inflater.inflate(R.layout.fragment_reports_admin, container, false);
+
+        tabLayout = view.findViewById(R.id.adminReportsTabLayout);
+        viewPager = view.findViewById(R.id.adminReportsViewPager);
+
+        AdminReportsPagerAdapter adapter = new AdminReportsPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+
+        return view;
     }
 
 }
