1054b6a4f17405cf814ebf1a1d25fe3bff3b0605
Nikola Mitrovic <mnikola300@gmail.com>
2024-06-02 02:40:46 +0200

    added accommodation reports

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation("com.android.car.ui:car-ui-lib:2.5.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("org.osmdroid:osmdroid-android:6.1.10")
-    implementation("com.github.lecho:hellocharts-library:1.5.8@aar")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/bookingapptim14/host/AccommodationReportsFragment.java
+++ app/src/main/java/com/example/bookingapptim14/host/AccommodationReportsFragment.java
@@ -1,25 +1,25 @@
 package com.example.bookingapptim14.host;
 
-import static android.content.Context.SENSOR_SERVICE;
-import static android.content.Context.VIBRATOR_SERVICE;
-
 import android.Manifest;
-import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.pdf.PdfDocument;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,6 +29,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
@@ -39,17 +40,27 @@ import com.example.bookingapptim14.Adapters.AccommodationReportsAdapter;
 import com.example.bookingapptim14.GlobalData;
 import com.example.bookingapptim14.R;
 import com.example.bookingapptim14.models.dtos.AccommodationDTO.AccommodationReportDTO;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static android.content.Context.SENSOR_SERVICE;
+import static android.content.Context.VIBRATOR_SERVICE;
+
 public class AccommodationReportsFragment extends Fragment {
+
     private static final String TAG = "AccommodationReportsFragment";
     private Vibrator vibrator;
     private SensorManager sensorManager;
@@ -57,7 +68,7 @@ public class AccommodationReportsFragment extends Fragment {
     private SensorEventListener proximityEventListener;
     private RecyclerView accommodationsRecyclerView;
     private AccommodationReportsAdapter adapter;
-
+    private PieChart pieChart;
     private List<AccommodationReportDTO> reports = new ArrayList<>();
 
     @Override
@@ -87,12 +98,13 @@ public class AccommodationReportsFragment extends Fragment {
 
             if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 // Generate PDF based on the date range
-                generatePdf(this.getContext(), reports);
+                generatePdf(getContext(), reports);
             } else {
                 requestStoragePermission();
             }
         });
 
+        pieChart = view.findViewById(R.id.profitPieChart);
         accommodationsRecyclerView = view.findViewById(R.id.hostAccommodationsRecyclerView);
         adapter = new AccommodationReportsAdapter(new ArrayList<>());
         accommodationsRecyclerView.setAdapter(adapter);
@@ -122,6 +134,116 @@ public class AccommodationReportsFragment extends Fragment {
 
         return view;
     }
+
+    private void showDatePickerDialog(EditText editText) {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(), (view, year1, month1, dayOfMonth) -> {
+            String date = dayOfMonth + "/" + (month1 + 1) + "/" + year1;
+            editText.setText(date);
+        }, year, month, day);
+        datePickerDialog.show();
+    }
+
+    private void requestStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                try {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                    intent.addCategory("android.intent.category.DEFAULT");
+                    intent.setData(Uri.parse(String.format("package:%s", this.getContext().getPackageName())));
+                    startActivityForResult(intent, 1);
+                } catch (Exception e) {
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                    startActivityForResult(intent, 1);
+                }
+            } else {
+                generatePdf(getContext(), reports);
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            } else {
+                generatePdf(getContext(), reports);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                generatePdf(getContext(), reports);
+            } else {
+                Toast.makeText(getContext(), "Permission denied to write to external storage", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                if (Environment.isExternalStorageManager()) {
+                    generatePdf(getContext(), reports);
+                } else {
+                    Toast.makeText(getContext(), "Permission denied to manage all files", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+
+    private List<AccommodationReportDTO> fetchReports(String startDate, String endDate) {
+        // For now, use test data
+        GlobalData data = GlobalData.getInstance();
+        List<AccommodationReportDTO> testAccommodations = data.getAccommodationReports();
+        adapter.setAccommodations(testAccommodations);
+        if (testAccommodations.isEmpty()) {
+            // Handle case when there are no reports available
+            return Collections.emptyList();
+        }
+
+        Map<String, Float> profitData = new HashMap<>();
+        for (AccommodationReportDTO accommodationReport : testAccommodations) {
+            profitData.put(accommodationReport.getAccommodationName(), (float) accommodationReport.getTotalProfit());
+        }
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        int i = 0;
+        for (Map.Entry<String, Float> entry : profitData.entrySet()) {
+            String accommodationName = entry.getKey();
+            Float profit = entry.getValue();
+            entries.add(new PieEntry(profit, accommodationName)); // Use accommodationName as label
+            i++;
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "Accommodation Profits");
+        dataSet.setColors(Color.rgb(255, 102, 0), Color.rgb(0, 204, 102), Color.rgb(0, 204, 255));
+        PieData dataPie = new PieData(dataSet);
+        dataPie.setValueFormatter(new PercentFormatter());
+        dataPie.setValueTextSize(11f);
+        dataPie.setValueTextColor(Color.WHITE);
+
+        pieChart.setData(dataPie);
+        pieChart.setUsePercentValues(true);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setTransparentCircleRadius(25f);
+        pieChart.setHoleRadius(25f);
+        pieChart.setRotationAngle(0);
+        pieChart.setRotationEnabled(true);
+        pieChart.animateY(1400);
+
+        return testAccommodations;
+    }
+
 
     public void generatePdf(Context context, List<AccommodationReportDTO> reports) {
         PdfDocument pdfDocument = new PdfDocument();
@@ -165,19 +287,20 @@ public class AccommodationReportsFragment extends Fragment {
             pageNumber++;
         }
 
-        // Save the PDF to external storage
         String fileName = "AccommodationReports.pdf";
-        File pdfFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+        File pdfFile;
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            // For Android 10 and above
+            pdfFile = new File(context.getExternalFilesDir(null), fileName);
+        } else {
+            // For Android 9 and below
+            pdfFile = new File(Environment.getExternalStorageDirectory(), fileName);
+        }
 
         try (FileOutputStream fos = new FileOutputStream(pdfFile)) {
             pdfDocument.writeTo(fos);
             Toast.makeText(context, "PDF saved successfully to " + pdfFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
-
-            // Notify media scanner
-            MediaScannerConnection.scanFile(context, new String[]{pdfFile.getAbsolutePath()}, null, (path, uri) -> {
-                Log.i(TAG, "Scanned " + path + ":");
-                Log.i(TAG, "-> uri=" + uri);
-            });
         } catch (IOException e) {
             Log.e(TAG, "Error saving PDF", e);
             Toast.makeText(context, "Error saving PDF: " + e.getMessage(), Toast.LENGTH_LONG).show();
@@ -186,64 +309,19 @@ public class AccommodationReportsFragment extends Fragment {
         }
     }
 
-    private void requestStoragePermission() {
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-        } else {
-            generatePdf(this.getContext(), reports);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                generatePdf(this.getContext(), reports);
-            } else {
-                Toast.makeText(getContext(), "Permission denied to write to external storage", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void showDatePickerDialog(EditText editText) {
-        final Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this.getContext(), (view, year1, month1, dayOfMonth) -> {
-            String date = dayOfMonth + "/" + (month1 + 1) + "/" + year1;
-            editText.setText(date);
-        }, year, month, day);
-        datePickerDialog.show();
-    }
-
     @Override
     public void onResume() {
         super.onResume();
-        sensorManager.registerListener(proximityEventListener, proximitySensor, SensorManager.SENSOR_DELAY_UI);
+        if (proximitySensor != null) {
+            sensorManager.registerListener(proximityEventListener, proximitySensor, SensorManager.SENSOR_DELAY_UI);
+        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(proximityEventListener);
-    }
-
-    private List<AccommodationReportDTO> fetchReports(String startDate, String endDate) {
-        //TODO endpoint!
-
-        // For now, use test data
-        GlobalData data = GlobalData.getInstance();
-        List<AccommodationReportDTO> testAccommodations = data.getAccommodationReports();
-        Map<String, Double> profitData = new HashMap<>();
-        for (AccommodationReportDTO accommodationReport:testAccommodations){
-            profitData.put(accommodationReport.getAccommodationName(),accommodationReport.getTotalProfit());
+        if (proximitySensor != null) {
+            sensorManager.unregisterListener(proximityEventListener);
         }
-
-        adapter.setAccommodations(testAccommodations);
-
-        return testAccommodations;
     }
 }
