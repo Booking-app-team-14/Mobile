4f34edaaedc26868a28e023ad8978cec997d579a
Vuk Dimitrov <vuk.dimitrov@gmail.com>
2024-06-11 23:39:58 +0200

    Shake event exception fix.

--- app/src/main/java/com/example/bookingapptim14/host/HomeFragmentHost.java
+++ app/src/main/java/com/example/bookingapptim14/host/HomeFragmentHost.java
@@ -211,13 +211,15 @@ public class HomeFragmentHost extends Fragment implements SensorEventListener {
     }
 
     private void filterAccommodationsByPriceDesc() {
-        if (accommodationsList == null) {
-            return;
-        }
-        List<SearchAccommodation> sortedList = new ArrayList<>(accommodationsList);
-        // GET request price/desc
-        Collections.sort(sortedList, (a1, a2) -> Float.compare((float)a2.getPrice(),(float)a1.getPrice()));
-        updateUI(sortedList);
+        try {
+            if (accommodationsList == null) {
+                return;
+            }
+            List<SearchAccommodation> sortedList = new ArrayList<>(accommodationsList);
+            // GET request price/desc
+            Collections.sort(sortedList, (a1, a2) -> Float.compare((float) a2.getPrice(), (float) a1.getPrice()));
+            updateUI(sortedList);
+        } catch (Exception e) { }
     }
 
     public void showFilterDialog() {
